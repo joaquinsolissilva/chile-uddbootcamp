c3c6060a4735bef74c531bfd0317e12fd1a495db
joaquinsolissilva <joaquinenero@gmail.com>
2022-01-24 19:33:39 -0300

    first commit

--- index.js
+++ index.js
@@ -1,42 +1,114 @@
 const delay = 3000
 
-const slides = document.querySelector('.slides');
+const slides1 = document.querySelector('.slides1');
 
-console.log('slides',slides);
+const slides2 = document.querySelector('.slides2');
 
-const slidesCount = slides.childElementCount;
+const slides3 = document.querySelector('.slides3');
 
-console.log('slidescount',slidesCount);
+console.log('slides',slides1);
 
-const maxLeft = (slidesCount -1) * 100 * -1
+const slidesCount1 = slides1.childElementCount;
 
-let current = 0;
+const slidesCount2 = slides2.childElementCount;
 
-function changeSlide(next = true) {
+const slidesCount3 = slides3.childElementCount;
+
+console.log('slidescount',slidesCount1);
+
+const maxLeft1 = (slidesCount1 -1) * 100 * -1
+
+const maxLeft2 = (slidesCount2 -1) * 100 * -1
+
+const maxLeft3 = (slidesCount3 -1) * 100 * -1
+
+console.log(maxLeft1)
+
+let current1 = 0;
+
+let current2 = 0;
+
+let current3 = 0;
+
+function changeSlide1(next = true) {
     if (next) {
-        current += current > maxLeft ? -100 : current*-1 ;
+        current1 += current1 > maxLeft1 ? -100 : current1*-1 ;
     } else {
-        current = current < 0 ? current + 100 : maxLeft ;
+        current1 = current1 < 0 ? current1 + 100 : maxLeft1 ;
     }
-    slides.style.left = `${current}%`
+    slides1.style.left = `${current1}%`
     //slides.style.left = current + "%"
 }
 
-let autoChange = setInterval(changeSlide,delay)
+function changeSlide2(next = true) {
+    if (next) {
+        current2 += current2 > maxLeft2 ? -100 : current2*-1 ;
+    } else {
+        current2 = current2 < 0 ? current2 + 100 : maxLeft2 ;
+    }
+    slides2.style.left = `${current2}%`
+    //slides.style.left = current + "%"
+}
 
-function restart() {
+function changeSlide3(next = true) {
+    if (next) {
+        current3 += current3 > maxLeft3 ? -100 : current3*-1 ;
+    } else {
+        current3 = current3 < 0 ? current3 + 100 : maxLeft3 ;
+    }
+    slides3.style.left = `${current3}%`
+    //slides.style.left = current + "%"
+}
+
+//let autoChange = setInterval(changeSlide,delay)
+
+/*function restart() {
     clearInterval(autoChange);
     autoChange = setInterval(changeSlide,delay);
-}
+}*/
 
 //controls
 
-document.querySelector(".next-slide").addEventListener("click", function(){
-    changeSlide();
+document.querySelector(".next-slide1").addEventListener("click", function(){
+    changeSlide1();
 //    restart();
 })
 
-document.querySelector(".prev-slide").addEventListener("click", function(){
-    changeSlide(false);
+document.querySelector(".prev-slide1").addEventListener("click", function(){
+    changeSlide1(false);
 //    restart();
+})
+
+document.querySelector(".next-slide2").addEventListener("click", function(){
+    changeSlide2();
+//    restart();
+})
+
+document.querySelector(".prev-slide2").addEventListener("click", function(){
+    changeSlide2(false);
+//    restart();
+})
+
+
+document.querySelector(".next-slide3").addEventListener("click", function(){
+    changeSlide3();
+//    restart();
+})
+
+document.querySelector(".prev-slide3").addEventListener("click", function(){
+    changeSlide3(false);
+//    restart();
+})
+
+
+document.querySelector("#hera").addEventListener("mouseover", function() {
+    console.log("mouseover")
+    let hero = document.querySelector("#hera")
+    hero.style.backgroundColor = "pink"
+})
+
+document.querySelector("#hera").addEventListener("mouseout", function() {
+    console.log("mouseover")
+    let hero = document.querySelector("#hera")
+    hero.style.backgroundColor = ""
 })
